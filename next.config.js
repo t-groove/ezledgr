353f228d0e8993a7fa0098ca89ef7be2b4670bb1
Thomas Gruver <thomasgruver@Mac.attlocal.net>
2026-03-28 16:47:28 -0500

    Fix avatar display: add Supabase domain to Next.js image config and use plain img tags

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'images.unsplash.com',
             },
+            {
+                protocol: 'https',
+                hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname,
+            },
         ],
     }
 };
